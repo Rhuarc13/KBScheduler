d7936a3d2025c490487d45fe083f6572732dcf3b
Weston Clark <clark.westonp@gmail.com>
2015-10-28 12:11:57 -0600

    Added the andoid app via version control

--- KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/LoginActivity.java
+++ KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/LoginActivity.java
@@ -3,53 +3,32 @@ package com.cs246.clark.mysqltestapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-/***********************************************************************
- *
- *                  ~~KnockerBall Schedule App~~
- *
- * This application is intended to serve as an interface to communicate
- * with a MySQL Database to create and store scheduling information for
- * the KnockerBall rental service. The app will provide users with a means
- * of scheduling a reserved time to rent the KnockerBalls and will express
- * those reservations on a master calendar for the renter to manage.
- *
- * 10/26/2015
- *
- * @author Weston Clark, Shem Sedrick, Jared Mefford
- * @version 1.0
- **********************************************************************/
-public class MainActivity extends AppCompatActivity {
-
-    //Button button = (Button) findViewById(R.id.registerButton);
-    TextView status;
+public class LoginActivity extends AppCompatActivity {
 
     EditText nameView;
     EditText passwordView;
+    EditText passwordConfirmView;
     EditText emailView;
 
-    /*********************************************************************
-    * On Create - sets the xml main layout
-    **********************************************************************/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
-
     /*********************************************************************
-    * Register 'button' connects to and sends data to the MySQL DB
+    * This will run the background task to communicate with the MySQL DB
     **********************************************************************/
-    public void registerNewUser(View view){
-        
+    public void registerNewUser(View view) {
         //set the TextViews
-        nameView     = (EditText) findViewById(R.id.name);
+        nameView = (EditText) findViewById(R.id.name);
         passwordView = (EditText) findViewById(R.id.password);
-        emailView    = (EditText) findViewById(R.id.email);
+        passwordConfirmView = (EditText) findViewById(R.id.passwordConfirm);
+        emailView = (EditText) findViewById(R.id.email);
 
         //set the data in the user class from the TextViews
         UserData user = new UserData();
@@ -60,16 +39,20 @@ public class MainActivity extends AppCompatActivity {
         //Set the info to local variables to send
         String name = user.getUser();
         String password = user.getPassword();
+        String passwordConfirm = passwordConfirmView.getText().toString();
         String email = user.getEmail();
 
         String method = "register";
 
-        //start a background thread to connect and communicate w/ the DB
-        BackgroundTask backgroundTask = new BackgroundTask(this);
-        backgroundTask.execute(method, name, password, email);
-
-    }
-
+        if(!password.equals(passwordConfirm)){
+            Toast.makeText(this, "Passwords do not match!", Toast.LENGTH_LONG).show();
+        } else {
+            //start a background thread to connect and communicate w/ the DB
+            BackgroundTask backgroundTask = new BackgroundTask(this);
+            backgroundTask.execute(method, name, password, email);
+            finish();
+        }
+     }
     /*********************************************************************
     * User Data - stores and sets the user information
     * In all honesty, we could do just fine without having this class,
@@ -93,3 +76,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+
+
+
---
+++ KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/MainActivity.java
@@ -0,0 +1,45 @@
+package com.cs246.clark.mysqltestapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/***********************************************************************
+ *
+ *                  ~~KnockerBall Schedule App~~
+ *
+ * This application is intended to serve as an interface to communicate
+ * with a MySQL Database to create and store scheduling information for
+ * the KnockerBall rental service. The app will provide users with a means
+ * of scheduling a reserved time to rent the KnockerBalls and will express
+ * those reservations on a master calendar for the renter to manage.
+ *
+ * 10/26/2015
+ *
+ * @author Weston Clark, Shem Sedrick, Jared Mefford
+ * @version 1.0
+ **********************************************************************/
+public class MainActivity extends AppCompatActivity {
+
+    /*********************************************************************
+    * On Create - sets the xml main layout
+    **********************************************************************/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /*********************************************************************
+    * Register 'button' takes you to the register activity
+    **********************************************************************/
+    public void registerButton(View view){
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
