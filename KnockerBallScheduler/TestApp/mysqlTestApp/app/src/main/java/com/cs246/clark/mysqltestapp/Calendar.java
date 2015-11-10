cb730df54a62d4cef5dfabc8f317e8619f58847c
Jared Mefford <mefford.ja@gmail.com>
2015-11-10 10:07:37 -0700

    Adding the Calendar activity

---
+++ KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/Calendar.java
@@ -0,0 +1,13 @@
+package com.cs246.clark.mysqltestapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Calendar extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar_activity);
+    }
+}
