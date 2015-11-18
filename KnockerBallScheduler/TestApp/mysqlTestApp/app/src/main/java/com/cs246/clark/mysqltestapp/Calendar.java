8100751607ff0ffa36ebe2fdd32fc2cc8d48457e
mefford.ja@gmail.com <mefford.ja@gmail.com>
2015-11-18 12:32:06 -0700

    Added LOG

--- KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/Calendar.java
+++ KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/Calendar.java
@@ -2,12 +2,19 @@ package com.cs246.clark.mysqltestapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Calendar extends Activity {
+
+    private static final String TAG = "CALENDAR";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_activity);
     }
+
+    Log.e(TAG, "You made a booboo");
+    Log.i(TAG, "You have selected a date");
+    Log.i(TAG, "You have selected a time");
 }
