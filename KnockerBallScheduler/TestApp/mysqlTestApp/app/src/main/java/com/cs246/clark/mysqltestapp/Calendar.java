18adb75da9a5cc027f950b1e8e759dbfa9f5cc8d
Shem Sedrick <ssedrick1@gmail.com>
2015-11-18 12:13:40 -0700

    I added a CalendarView variable for interacting with the calendar

--- KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/Calendar.java
+++ KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/Calendar.java
@@ -2,12 +2,17 @@ package com.cs246.clark.mysqltestapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CalendarView;
 
 public class Calendar extends Activity {
+    CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_activity);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
     }
+
 }
