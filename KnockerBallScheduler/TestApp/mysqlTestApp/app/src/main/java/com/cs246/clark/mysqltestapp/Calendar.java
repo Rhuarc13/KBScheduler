9371eeca399fa1e20a0c7f75ba6ad1b1b5269417
Weston Clark <clark.westonp@gmail.com>
2015-11-19 19:57:18 -0700

    Added a temp fix for the calendar - pushing the sign in button will take you to the calendar without verifying the password/user email. This is set up like this at the moment for testing purposes

--- KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/Calendar.java
+++ KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/Calendar.java
@@ -9,5 +9,9 @@ public class Calendar extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_activity);
+
+
+
+
     }
 }
