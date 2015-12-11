f232583ff42c51dd22384b587fd2464fa3bf7783
Weston Clark <clark.westonp@gmail.com>
2015-12-10 22:35:55 -0700

    toastMyGoat

--- KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/DayView.java
+++ KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/DayView.java
@@ -2,20 +2,10 @@ package com.cs246.clark.mysqltestapp;
 
 import android.app.Activity;
 import android.content.Intent;
-<<<<<<< HEAD
-import android.content.SharedPreferences;
-=======
->>>>>>> 2f90c66e6c1f241d452b44b3fcb50dd54a9b12ec
-import android.net.Uri;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-<<<<<<< HEAD
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-=======
->>>>>>> 2f90c66e6c1f241d452b44b3fcb50dd54a9b12ec
 import android.widget.Toast;
 
 
@@ -23,21 +13,33 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import android.widget.TextView;
-<<<<<<< HEAD
 
-import java.util.ArrayList;
-=======
->>>>>>> 2f90c66e6c1f241d452b44b3fcb50dd54a9b12ec
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class DayView extends Activity {
 
     private final static String TAG = "DAY_VIEW";
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
+    private final static HashMap<Integer, String> TIMELORD = new HashMap<Integer, String>(){
+        {
+            put(0, "default");
+            put(1, "six");
+            put(2, "sixT");
+            put(3, "seven");
+            put(4, "sevenT");
+            put(5, "eight");
+            put(6, "eightT");
+            put(7, "nine");
+            put(8, "nineT");
+            put(9, "ten");
+            put(10, "eight");
+            put(11, "eight");
+            put(12, "eight");
+        }
+    };
 
 
     @Override
@@ -87,10 +89,6 @@ public class DayView extends Activity {
         BackgroundTask backgroundTask = new BackgroundTask(date, method, r);
         backgroundTask.execute();
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 2f90c66e6c1f241d452b44b3fcb50dd54a9b12ec
         Lock lock = new ReentrantLock();
         int waitTime = 0;
         synchronized (lock) {
@@ -110,54 +108,179 @@ public class DayView extends Activity {
             }*/
         }
 
-<<<<<<< HEAD
-        String timeView = "sevenT";
+        int start_hr, start_min, end_hr, end_min = 0;
 
-        Resources res = getResources();
-        int id = res.getIdentifier(timeView, "id", this.getPackageName());
-        TextView tempTextView = (TextView) findViewById(id);
-
-=======
-
-        String startTime;
->>>>>>> 2f90c66e6c1f241d452b44b3fcb50dd54a9b12ec
         if (r.getCode() == 200) {
             Log.i(TAG, "Response is: " + r.getText());
             try {
                 JSONObject jsonObject = new JSONObject(r.getText());
                 JSONArray array = jsonObject.getJSONArray("times");
-                for (int i = 0; i < array.length(); i++) {
+                for (int i = 0; i < array.length() - 1; i++) {
                     JSONObject times = array.getJSONObject(i);
                     Log.i(TAG, times.toString());
-                    startTime = times.getString("start_time");
-                    int start_hr = Integer.parseInt(startTime.split(":")[0]);
-                    int start_min = Integer.parseInt(startTime.split(":")[1]);
-                    startTime = start_hr + ":" + start_min;
+                    String startTime = times.getString("start_time");
+                    start_hr = Integer.parseInt(startTime.split(":")[0]);
+                    start_min = Integer.parseInt(startTime.split(":")[1]);
+                    if (start_hr > 12) {
+                        start_hr = start_hr - 12;
+                    }
+
                     String endTime   = times.getString("end_time");
+                    end_hr = Integer.parseInt(endTime.split(":")[0]);
+                    if (end_hr > 12) {
+                         end_hr = end_hr - 12;
+                    }
+                    end_min = Integer.parseInt(endTime.split(":")[1]);
+
+
+                    Log.i(TAG, "start time: " + start_hr + ":" + start_min);
+                    Log.i(TAG, "end time: " + end_hr + ":" + end_min);
+
+                    String timeView = "";
+                    int index = 0;
+                    int eindex = 0;
+
+                    for (int j = 0; j < 2; j++) {
+                        int target1 = 0;
+                        int target2 = 0;
+                        if (j == 0) {
+                            target1 = start_hr;
+                            target2 = start_min;
+                        } else {
+                            target1 = end_hr;
+                            target2 = end_min;
+                        }
+                        switch (target1) {
+                            case 6:
+                                if (target2 < 30) {
+                                    if (j == 0) {
+                                        index = 1;
+                                    } else {
+                                        eindex = 1;
+                                    }
+                                } else {
+                                    if (j == 0) {
+                                        index = 2;
+                                    } else {
+                                        eindex = 2;
+                                    }
+                                }
+                                break;
+                            case 7:
+                                if (target2 < 30) {
+                                    if (j == 0) {
+                                        index = 3;
+                                    } else {
+                                        eindex = 3;
+                                    }
+                                } else {
+                                    if (j == 0) {
+                                        index = 4;
+                                    } else {
+                                        eindex = 4;
+                                    }
+                                }
+                                break;
+                            case 8:
+                                if (target2 < 30) {
+                                    if (j == 0) {
+                                        index = 5;
+                                    } else {
+                                        eindex = 5;
+                                    }
+                                } else {
+                                    if (j == 0) {
+                                        index = 6;
+                                    } else {
+                                        eindex = 6;
+                                    }
+                                }
+                                break;
+                            case 9:
+                                if (target2 < 30) {
+                                    if (j == 0) {
+                                        index = 7;
+                                    } else {
+                                        eindex = 7;
+                                    }
+                                } else {
+                                    if (j == 0) {
+                                        index = 8;
+                                    } else {
+                                        eindex = 8;
+                                    }
+                                }
+                                break;
+                            case 10:
+                                if (target2 < 30) {
+                                    if (j == 0) {
+                                        index = 9;
+                                    } else {
+                                        eindex = 9;
+                                    }
+                                } else {
+                                    if (j == 0) {
+                                        index = 10;
+                                    } else {
+                                        eindex = 10;
+                                    }
+                                }
+                                break;
+                            case 11:
+                                if (target2 < 30) {
+                                    if (j == 0) {
+                                        index = 11;
+                                    } else {
+                                        eindex = 11;
+                                    }
+                                } else {
+                                    if (j == 0) {
+                                        index = 12;
+                                    } else {
+                                        eindex = 12;
+                                    }
+                                }
+                                break;
+                            default:
+                                index = 0;
+                                eindex = 0;
+                                break;
+                        }
+                    }
+
+
+                    for (int j = index; j < eindex; j++) {
+                        timeView = TIMELORD.get(j);
+                        Log.i(TAG, "timeView name = " + timeView + ": indexes: " + index + "/" + eindex);
+                        Resources res = getResources();
+                        int id = res.getIdentifier(timeView, "id", this.getPackageName());
+
+                        TextView tempTextView = (TextView) findViewById(id);
+                        if (tempTextView != null) {
+                            tempTextView.setBackgroundColor(getResources().getColor(R.color.timeSlotReserved));
+                            tempTextView.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Toast.makeText(getApplicationContext(), R.string.no_click, Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        } else {
+                            Log.e(TAG, "tempTextView was null... :(");
+                        }
+                    }
+                //here is where we need to set the color/click-ability of the time slots
                 }
             } catch (JSONException je) {
                 Log.e(TAG, "JSONException: " + je.toString());
             }
-        } else {
-            Toast.makeText(this, "Error occurred connecting to the database", Toast.LENGTH_LONG).show();
-        }
-
-        String timeView = "sevenT";
-
-        Resources res = getResources();
-        int id = res.getIdentifier(timeView, "id", this.getPackageName());
 
 
-        TextView tempTextView = (TextView) findViewById(id);
-        tempTextView.setText("LALALALA SUCCESS!");
-        //here is where we need to set the color/click-ability of the time slots
-
-<<<<<<< HEAD
-}
-=======
+            } else {
+                Toast.makeText(this, "Error occurred connecting to the database", Toast.LENGTH_SHORT).show();
+            }
 
     }
->>>>>>> 2f90c66e6c1f241d452b44b3fcb50dd54a9b12ec
+
 
     public void onClick(View view) {
         Intent intent = new Intent(this, Confermation.class);
