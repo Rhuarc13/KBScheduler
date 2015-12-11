d85672af79c68621346f11b47d74b9c43efdadea
Rhuarc <ssedrick1@gmail.com>
2015-12-10 20:42:33 -0700

    Got the JSONObject to get the response and parse it somewhat

--- KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/DayView.java
+++ KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/DayView.java
@@ -3,16 +3,34 @@ package com.cs246.clark.mysqltestapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class DayView extends Activity {
 
-
+    private final static String TAG = "DAY_VIEW";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,58 +38,136 @@ public class DayView extends Activity {
         setContentView(R.layout.day_view);
 
 
-
         String date = getIntent().getStringExtra("date");
 
         String dateList[] = date.split("\\s");
 
         String method = "pull_time";
-        String year   = dateList[5];
-        String month  = dateList[1];
-        String day    = dateList[2];
+        String year = dateList[5];
+        String month = dateList[1];
+        String day = dateList[2];
 
-        if(month.equals("Jan")){
+        if (month.equals("Jan")) {
             month = "01";
-        } else if(month.equals("Feb")){
+        } else if (month.equals("Feb")) {
             month = "02";
-        } else if(month.equals("Mar")){
+        } else if (month.equals("Mar")) {
             month = "03";
-        } else if(month.equals("Apr")){
+        } else if (month.equals("Apr")) {
             month = "04";
-        } else if(month.equals("May")){
+        } else if (month.equals("May")) {
             month = "05";
-        } else if(month.equals("Jun")){
+        } else if (month.equals("Jun")) {
             month = "06";
-        } else if(month.equals("Jul")){
+        } else if (month.equals("Jul")) {
             month = "07";
-        } else if(month.equals("Aug")){
+        } else if (month.equals("Aug")) {
             month = "08";
-        } else if(month.equals("Sep")){
+        } else if (month.equals("Sep")) {
             month = "09";
-        } else if(month.equals("Oct")){
+        } else if (month.equals("Oct")) {
             month = "10";
-        } else if(month.equals("Nov")){
+        } else if (month.equals("Nov")) {
             month = "11";
-        } else if(month.equals("Dec")){
+        } else if (month.equals("Dec")) {
             month = "12";
         }
 
-        date = year+"-"+month+"-"+day;
+        date = year + "-" + month + "-" + day;
 
-        BackgroundTask backgroundTask = new BackgroundTask(date, method);
+        Response r = new Response();
+        BackgroundTask backgroundTask = new BackgroundTask(date, method, r);
         backgroundTask.execute();
 
+        Lock lock = new ReentrantLock();
+        int waitTime = 0;
+        synchronized (lock) {
+            while (r.getCode() == 0 && waitTime < 26) {
+                try {
+                    lock.wait(100);
+                    waitTime++;
+                } catch (InterruptedException e) {
+                    Log.e(TAG, "Ran into an InterruptedException");
+                }
+            }
 
+            /*try {
+                lock.wait(500);
+            } catch (InterruptedException e) {
+                Log.e(TAG, "Ran into an InterruptedException");
+            }*/
+        }
+
+        if (r.getCode() == 200) {
+            Log.i(TAG, "Response is: " + r.getText());
+            try {
+                JSONObject jsonObject = new JSONObject(r.getText());
+                JSONArray array = jsonObject.getJSONArray("times");
+                for (int i = 0; i < array.length(); i++) {
+                    JSONObject times = array.getJSONObject(i);
+                    Log.i(TAG, times.toString());
+                    String startTime = times.getString("start_time");
+                    String endTime   = times.getString("end_time");
+                }
+            } catch (JSONException je) {
+                Log.e(TAG, "JSONException: " + je.toString());
+            }
+        } else {
+            Toast.makeText(this, "Error occurred connecting to the database", Toast.LENGTH_LONG).show();
+        }
 
 
         //here is where we need to set the color/click-ability of the time slots
 
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public void onClick(View view) {
         Intent intent = new Intent(this, Confermation.class);
         startActivity(intent);
         finish();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "DayView Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.cs246.clark.mysqltestapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "DayView Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.cs246.clark.mysqltestapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
