2a67e775d6a0e110713e4b284e6b3d1722240645
mefford.ja@gmail.com <mefford.ja@gmail.com>
2015-12-10 19:24:35 -0700

    set up the confirmation activity

--- KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/Confermation.java
+++ KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/Confermation.java
@@ -2,12 +2,20 @@ package com.cs246.clark.mysqltestapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Confermation extends Activity {
+    TextView custEmail;
+    String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confermation);
+        custEmail = (TextView) findViewById(R.id.customerEmail);
+        email = getPreferences(0).getString("email", "");
+        custEmail.setText(email);
+        System.out.println("The email is :" + email);
+
     }
 }
--- KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/DayView.java
+++ KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/DayView.java
@@ -1,7 +1,10 @@
 package com.cs246.clark.mysqltestapp;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 
 public class DayView extends Activity {
@@ -16,5 +19,11 @@ public class DayView extends Activity {
         String date = getIntent().getStringExtra("date");
         System.out.println(date);
 
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this, Confermation.class);
+        startActivity(intent);
+        finish();
     }
 }
