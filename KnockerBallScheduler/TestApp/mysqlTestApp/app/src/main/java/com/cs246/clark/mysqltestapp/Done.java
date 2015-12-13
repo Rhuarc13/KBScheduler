e19b1e7319f3fedfc0c4645e94f431d86bd02341
Rhuarc <ssedrick1@gmail.com>
2015-12-12 21:40:17 -0700

    set up for the email

--- KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/Done.java
+++ KnockerBallScheduler/TestApp/mysqlTestApp/app/src/main/java/com/cs246/clark/mysqltestapp/Done.java
@@ -11,6 +11,12 @@ public class Done extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_done);
+
+        Intent old = getIntent();
+        
+
+        BackgroundTask backgroundTask = new BackgroundTask("email", old.getStringExtra("first_name"), old.getStringExtra("last_name"), old.getStringExtra("email"), old.getStringExtra("date"), old.getStringExtra("start"), old.getStringExtra("end"), old.getStringExtra("street"), old.getStringExtra("city"), old.getStringExtra("state"), old.getStringExtra("duration"));
+        backgroundTask.execute();
     }
 
     public void onClickDone (View view) {
